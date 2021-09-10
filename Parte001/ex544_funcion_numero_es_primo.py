a8261246f509c7035ae1608d68979ecd0be0e68d
A00835208 <88891270+A00835208@users.noreply.github.com>
2021-09-10 08:18:40 -0500

    Update ex544_funcion_numero_es_primo.py

--- Parte001/ex544_funcion_numero_es_primo.py
+++ Parte001/ex544_funcion_numero_es_primo.py
@@ -1,14 +1,6 @@
 # Ejercicio 544: Crear una función para comprobar si un número dado es primo.
 
-def es_primo(numero):
-    if numero == 1:
-        return False
-    elif numero == 2:
-        return True
-    else:
-        for i in range(2, numero):
-            if numero % i == 0:
-                return False
+
         
         return True
 
